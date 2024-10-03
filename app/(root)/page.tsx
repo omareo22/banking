25b1eae6b7523bd11a37e7e569d41df4842cb33b
Omar <omar.mahmoud32@gmail.com>
2024-10-03 17:16:20 -0400

    Added Counter and DonutChart for Home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
+
+    const loggedIn = {firstName: 'Omar'}
   return (
-    <div>Home</div>
+    <section className='home'>Home
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox
+                    type='greeting'
+                    title='Welcome'
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext = 'Access and manage your account and transcations'
+                />
+
+                <TotalBalanceBox 
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1200.35}
+
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
